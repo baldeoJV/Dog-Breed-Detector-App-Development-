778c59b552acfa1737a8eeeab723639d8bf12f86
Carl <carlarvinhipolito14@gmail.com>
2025-05-18 17:42:32 +0800

    added a dependency for fetching data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.espresso.intents)
+    implementation("com.android.volley:volley:1.2.1") //for fetching data
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
